6e264350c6dddd9fb395a5744fca9382fe89b8f2
Peterlee <peterlee0127@gmail.com>
2014-04-27 23:33:46 +0800

    init iOS project

---
+++ iOS/DB_Project/DB_Project/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  DB_Project
+//
+//  Created by Peterlee on 4/27/14.
+//  Copyright (c) 2014 Peterlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ iOS/DB_Project/DB_Project/MainViewController.m
@@ -0,0 +1,33 @@
+//
+//  MainViewController.m
+//  DB_Project
+//
+//  Created by Peterlee on 4/27/14.
+//  Copyright (c) 2014 Peterlee. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+@end
