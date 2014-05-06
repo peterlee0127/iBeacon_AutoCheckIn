8c1262ca4c74682a06dcd884fbd23ca0ae7e9d68
Peterlee <peterlee0127@gmail.com>
2014-05-06 14:51:13 +0800

    fix connect mongo

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,24 +8,24 @@ var debug = require('debug')('my-application');
 var mongoose = require('mongoose');
 
 
-// var session = require('express-session');
-// var MongoStore = require('connect-mongo')(session);
+//var session = require('express-session');
+//var MongoStore = require('connect-mongo')(session);
 
 // view engine setup
 app.set('view engine', 'ejs');
 
-app.use(favicon());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/stylesheets')));
+
 /*
 app.use(session({
-		secret: settings.cookie_secret,
+		secret: "324rgrgegr",
 		store: new MongoStore({
-				db: db_hw_session.db
+				db: "iBeaconCheckInSession",
 		})
 }));
 */
