759095e7b660aa195b53f7fce1052676eae1243c
Peterlee <peterlee0127@gmail.com>
2014-05-16 15:00:01 +0800

    add char room prototype

--- Server/public/core.js
+++ Server/public/core.js
@@ -101,18 +101,71 @@ function mainController($scope, $http) {
 
 		socket.on('connect', function(data) {
 
-			console.log("socket connected");
 
 		});
 
 
 		socket.on('disconnect', function(data) {
 
-			console.log("socket disconnected");
-
 		});
 
 		socket.on('reloadData', function(data){
 				$scope.reloadData();
 		});
+
+		socket.on('distance', function(data){
+
+
+		});
+
+		socket.on('chat',function(data){
+
+
+	 });
+
+	socket.on('listen_chat',function(data) {
+	var e = $('<li class="other_chat">'+
+				'<div class="chat-body clearfix">'+
+					'<div class="chat_id">'+
+						'<strong class="primary-font chat_name">'+data.stu_id+'</strong>'+
+							'<small class="pull-right text-muted">'+
+								'<span class="glyphicon glyphicon-time"></span>'+since+
+							'</small>'+
+						'</div>'+
+					'<p>'+data.message+'</p>'+
+				'</div>'+
+			'</li>');
+	$('#ListenChat').append(e);
+	var objDiv = document.getElementById("chat_box_outer");
+	objDiv.scrollTop = objDiv.scrollHeight;
+});
+
+function getDateTime() {
+    var now     = new Date();
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1;
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds();
+    if(month.toString().length == 1) {
+        month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+        day = '0'+day;
+    }
+    if(hour.toString().length == 1) {
+        hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+        minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+        second = '0'+second;
+    }
+    var dateTime = hour+':'+minute+':'+second;
+    return dateTime;
+}
+
+
 }
