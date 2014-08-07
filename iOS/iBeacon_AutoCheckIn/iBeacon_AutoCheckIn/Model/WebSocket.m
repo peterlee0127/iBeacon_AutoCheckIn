2e82d496e620a36fe9032cfd07f61c596439aa40
Peterlee <peterlee0127@gmail.com>
2014-08-07 22:14:10 +0800

    update

--- iOS/iBeacon_AutoCheckIn/iBeacon_AutoCheckIn/Model/WebSocket.m
+++ iOS/iBeacon_AutoCheckIn/iBeacon_AutoCheckIn/Model/WebSocket.m
@@ -49,8 +49,8 @@
         return;
     if(self.webSocket.isConnected)
         return;
-    if(![iBeaconModel shareInstance].TouchIDAuth)
-        return;
+//    if(![iBeaconModel shareInstance].TouchIDAuth)
+//        return;
     
     self.webSocket=[[SocketIO alloc] initWithDelegate:self];
     [self.webSocket connectToHost:defaultServer onPort:[defaultPort integerValue]];
