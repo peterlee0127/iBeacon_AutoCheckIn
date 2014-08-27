8316f4d0e0ed43f370189d016760fb41d785c27c
Peterlee <peterlee0127@gmail.com>
2014-08-27 11:04:38 +0800

    test

--- Server/socket.js
+++ Server/socket.js
@@ -150,8 +150,9 @@ module.exports =  function(io){
                     {
                           if( (student.out[i].getTime()-student.in[i].getTime() )<10*1000){
                               console.log("in/out is too close");
-							                student.in.pull({ _id:  student._id });
-							                student.out.pull({ _id:  student._id });
+                              student.in.remove(uid);
+                              student.out.remove(uid);
+                              student.save();
 
                           }
                     }
