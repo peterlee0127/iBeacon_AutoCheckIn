093898fa6a3f246a25189474a6aa2e53a2f6e328
Peterlee <peterlee0127@gmail.com>
2014-08-27 18:45:41 +0800

    test

--- Server/public/core.js
+++ Server/public/core.js
@@ -105,7 +105,6 @@ angular.module('PeopleList', [])
 
 	$scope.deleteRecord = function(stu_id,index) {
 		var post = {  "stu_id":stu_id,'index':index };
-		console.log(post);
 		$http.post('/api/deleteRecord/' , JSON.stringify(post))
 			.success(function(data) {
 				$scope.reloadData();
